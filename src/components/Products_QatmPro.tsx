0eb176810793444badd9307066375d6444754592
Matt Glenn <highopenended@gmail.com>
2025-12-14 16:01:26 -0500

    WIP

--- src/components/Products_QatmPro.tsx
+++ src/components/Products_QatmPro.tsx
@@ -17,55 +17,53 @@ import ClickableProduct from "./clickableProducts/ClickableProduct";
 
 const Products_QatmPro = () => {
     return (
-        <section
-            className="products-qatm-pro-section"
-            style={{ backgroundImage: `url(${backgroundImage})` }}
-        >
+        <section className="products-qatm-pro-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <div className="products-header">
+                <h2>Products</h2>
+            </div>
             <div className="products-container">
-            <ClickableProduct
-  src={emulsionImage}
-  alt="5QATM PRO Emulsion"
-  href="https://item.rakuten.co.jp/24quantum/24emulsion200g/"
-  className="pro-product pro-product-emulsion"
-  scale={1.3}
-/>
+                <ClickableProduct
+                    src={emulsionImage}
+                    alt="5QATM PRO Emulsion"
+                    href="https://item.rakuten.co.jp/24quantum/24emulsion200g/"
+                    className="pro-product pro-product-emulsion"
+                    scale={1.3}
+                />
 
-<ClickableProduct
-  src={shampooImage}
-  alt="5QATM PRO Shampoo"
-  href="https://item.rakuten.co.jp/24quantum/5shampoo300ml/"
-  className="pro-product pro-product-shampoo"
-  scale={1.5}
-/>
+                <ClickableProduct
+                    src={shampooImage}
+                    alt="5QATM PRO Shampoo"
+                    href="https://item.rakuten.co.jp/24quantum/5shampoo300ml/"
+                    className="pro-product pro-product-shampoo"
+                    scale={1.5}
+                />
 
-<ClickableProduct
-  src={treatmentImage}
-  alt="5QATM PRO Treatment"
-  href="https://item.rakuten.co.jp/24quantum/5treatment300g/"
-  className="pro-product pro-product-treatment"
-  scale={1.6}
-/>
+                <ClickableProduct
+                    src={treatmentImage}
+                    alt="5QATM PRO Treatment"
+                    href="https://item.rakuten.co.jp/24quantum/5treatment300g/"
+                    className="pro-product pro-product-treatment"
+                    scale={1.6}
+                />
 
-<ClickableProduct
-  src={rebirthImage}
-  alt="5QATM PRO Rebirth"
-  href="https://item.rakuten.co.jp/24quantum/5rebirth200g/"
-  className="pro-product pro-product-rebirth"
-  scale={1.0}
-/>
+                <ClickableProduct
+                    src={rebirthImage}
+                    alt="5QATM PRO Rebirth"
+                    href="https://item.rakuten.co.jp/24quantum/5rebirth200g/"
+                    className="pro-product pro-product-rebirth"
+                    scale={1.0}
+                />
 
-<ClickableProduct
-  src={coreImage}
-  alt="5QATM PRO Core"
-  href="https://item.rakuten.co.jp/24quantum/5core200g/"
-  className="pro-product pro-product-core"
-  scale={1.2}
-/>
-
+                <ClickableProduct
+                    src={coreImage}
+                    alt="5QATM PRO Core"
+                    href="https://item.rakuten.co.jp/24quantum/5core200g/"
+                    className="pro-product pro-product-core"
+                    scale={1.2}
+                />
             </div>
         </section>
     );
 };
 
 export default Products_QatmPro;
-
