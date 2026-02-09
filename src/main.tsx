34bbb6719bb89f0a48ba48f303e3ef1571a1c13c
Matt Glenn <highopenended@gmail.com>
2026-02-08 19:18:38 -0500

    Started shifting over to vite-react-ssg for SEO purposes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { ViteReactSSG } from 'vite-react-ssg/single-page'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+export const createRoot = ViteReactSSG(
+  <App />
 )
